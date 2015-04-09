03ab622fb6b64b3e05caca045549726a8ec81532
VirgieLynF09 <virgielyn_fernandez@yahoo.ca>
2015-04-09 17:38:13 -0400

    database string

--- db.js
+++ db.js
@@ -1,4 +1,4 @@
 module.exports = {
-	//'url' : 'mongodb://virgief09:virgief09@ds061741.mongolab.com:61741/assign2'
-	'url' : 'mongodb://localhost/assignment2'
+	'url' : 'mongodb://virgief09:virgief09@ds061741.mongolab.com:61741/assign2'
+//	'url' : 'mongodb://localhost/assignment2'
 }
